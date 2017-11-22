2d4a99259dd0d545de676ff4e90c7b35bd41b9ee
Bakaev-MM <bakaev-mm@gamma-center.ru>
2017-11-22 18:04:43 +0300

    new_dz

--- info.py
+++ info.py
@@ -1,6 +1,5 @@
 print("Введите своё имя: ")
 first_name = input()
-user_info = {"first_name": first_name, "last_name": ""}
 print("Введите свою фамилию: ")
 last_name = input()
 user_info = {"first_name": first_name, "last_name": last_name}
---
+++ my_test.py
@@ -0,0 +1,6 @@
+user_info = {"first_name" : "Андрей" , "last_name" : "Саликов"}
+f_name = str(input("Введите имя: "))
+user_info["first_name"] = f_name
+l_name = str(input("Введите фамилию: "))
+user_info["last_name"] = l_name
+print(user_info)
