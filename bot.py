5a6ab15e0be76796c88defea8dceb9dacc4008d0
Bakaev-MM <bakaev-mm@gamma-center.ru>
2017-11-24 14:49:27 +0300

    Доработкка бота

--- bot.py
+++ bot.py
@@ -1,29 +1,32 @@
 # Импортируем нужные компоненты
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import logging
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+import logging, settings
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
                     )
 
 # Функция, которая соединяется с платформой Telegram, "тело" нашего бота
-def greet_user(bot, update):
-    text = 'Вызван /start'
+def start_bot(bot, update):
+    text = '''Привет {}!
+Я простой бот и понимаю команду /start'''.format(update.message.chat.first_name)
     logging.info(text)
     update.message.reply_text(text)
 
-def talk_to_me(bot, update):
+def chat(bot, update):
     user_text = update.message.text 
     logging.info(user_text)
     update.message.reply_text(user_text)
 
 def main():
-    updater = Updater("504501242:AAE7SY5VxEXM81HKpXBqJZIGmtmfuPydQiA")
+    updater = Updater(settings.TELEGRAM_API_KEY)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler("start", start_bot))
+    dp.add_handler(MessageHandler(Filters.text, chat))
     updater.start_polling()
     updater.idle()
 
 # Вызываем функцию - эта строчка собственно запускает бота
-main()
+if __name__ == '__main__':
+    logging.info('bot_started')
+    main()
--- my_test.py
+++ my_test.py
@@ -1,6 +1,8 @@
-user_info = {"first_name" : "Андрей" , "last_name" : "Саликов"}
-f_name = str(input("Введите имя: "))
-user_info["first_name"] = f_name
-l_name = str(input("Введите фамилию: "))
-user_info["last_name"] = l_name
-print(user_info)
+def cut_cake(parts):
+    try:
+        return 1/int(parts)
+    except (ZeroDivisionError, TypeError, ValueError):
+        return "С ума посходили?"
+
+cake = cut_cake([1, 2, 3])
+print(cake)
